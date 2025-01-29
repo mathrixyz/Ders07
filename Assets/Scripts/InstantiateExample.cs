942131846e29cfaf5c210cdba6d8c99569df0de1
mathrixyz <cinaryavuz@gmail.com>
2025-01-30 01:18:17 +0300

    2

---
+++ Assets/Scripts/InstantiateExample.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class InstantiateExample: MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnObject;
+    [SerializeField] private Transform _parentObject;
+
+    private void Start()
+    {
+        Instantiate( _spawnObject,_parentObject);
+    }
+}
